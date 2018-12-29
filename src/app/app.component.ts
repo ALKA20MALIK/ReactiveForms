8ec7fe402617287a0d94d53c14fa0d80138ee3da
ALKA <ALKA20MALIK>
2018-12-30 01:20:00 +0530

    30/12/2018 1:19AM

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  signupForm:FormGroup;
+  forbiddenUsername=['anna','mana'];
+  ngOnInit(){
+  this.signupForm=new FormGroup({
+    'userData':new FormGroup({
+      'username':new FormControl(null,[Validators.required,this.forbiddenNames.bind(this)]),
+      'email':new FormControl(null,[Validators.required,Validators.email]),
+    }),
+    'gender':new FormControl('female'),
+    'hobbies':new FormArray([])
+  });
+  }
+  onAddHobby(){
+    const control=new FormControl(null,Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control:FormControl):{[s:string]:Boolean}{
+    if(this.forbiddenUsername.indexOf(control.value)!==-1){
+      console.log(this.forbiddenUsername.indexOf(control.value)!==-1);
+      return {'nameIsForbidden':true};
+    }
+    return null;
+  }
+}
