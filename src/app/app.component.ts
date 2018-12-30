77ce9fec06b018260f001fbce6e3b75835ced3ae
ALKA <ALKA20MALIK>
2018-12-30 14:15:31 +0530

    30/12/2018 2:15 PM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { resolve, reject } from 'q';
+import { getMaxListeners } from 'cluster';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +10,37 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+ 
   genders = ['male', 'female'];
-  signupForm:FormGroup;
+  @ViewChild('f') signupForm:FormGroup;
   forbiddenUsername=['anna','mana'];
+  
   ngOnInit(){
   this.signupForm=new FormGroup({
     'userData':new FormGroup({
       'username':new FormControl(null,[Validators.required,this.forbiddenNames.bind(this)]),
-      'email':new FormControl(null,[Validators.required,Validators.email]),
+      'email':new FormControl(null,[Validators.required,Validators.email],this.forbiddenEmails),
     }),
     'gender':new FormControl('female'),
     'hobbies':new FormArray([])
   });
+
+  this.signupForm.patchValue({
+    'userData':{'username':'alka','email':'alka@gmail.com'},
+    'gender':'female',
+  });
+  this.signupForm.setControl('hobbies', new FormArray([
+    new FormControl('music'),
+    new FormControl('sports')
+  ]));
+  // this.signupForm.valueChanges
+  //   .subscribe((value)=>{
+  //     console.log(value);
+  //   });
+  this.signupForm.statusChanges
+    .subscribe((status)=>{
+      console.log(status);
+    });
   }
   onAddHobby(){
     const control=new FormControl(null,Validators.required);
@@ -31,5 +53,22 @@ export class AppComponent implements OnInit{
       return {'nameIsForbidden':true};
     }
     return null;
+  }
+
+  forbiddenEmails(control:FormControl):Promise<any> | Observable<any>{
+    const promise=new Promise<any>((resolve,reject)=>{
+      setTimeout(()=>{
+        if(control.value==='malikalka2090@gmail.com'){
+          resolve({'emailIsForbidden':true});
+        }
+        else{
+          resolve(null);
+        }
+      },1500)
+    });
+    return promise;
+  }
+  onSubmit(){
+    console.log(this.signupForm);
   }
 }
